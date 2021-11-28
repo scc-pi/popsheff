57898c80a999cee11878cd58eb38fd77f90ee326
laurie-platt <laurieplatt@hotmail.com>
2021-11-28 15:27:17 +0000

    start LA data rewrite

--- R/data.R
+++ R/data.R
@@ -1,3 +1,10 @@
+#' Sheffield population from 2001 to 2020.
+#'
+#' Sheffield population estimates provided by the ONS.
+#'
+#' @format A data frame with two variables: \code{Year} and \code{Population}.
+"pop_la_yrs"
+
 #' Population of Sheffield LSOAs by fifteen year age bands.
 #'
 #' Sheffield population provided by the ONS. 2020 mid-year estimates.
---
+++ data-raw/pop_la.R
@@ -0,0 +1,118 @@
+# HEADER ---------------------
+# Description: Get the Local Authority population estimates for Sheffield
+#               from 2001 to 2020
+# Author: Laurie Platt
+
+# SETUP -------------------------
+library(tidyverse)
+library(readxl)
+
+## Local variables --------------
+
+# Location of data we're reading
+data_in_folder <- "data-raw"
+
+# Location of data we're creating
+data_out_folder <- "data"
+
+# Name of the file with population estimates at LA level
+pop_la_file <- "ukpopestimatesmid2020on2021geography.xls"
+
+# Name of the sheet with LA population from 2001 to 2020
+pop_yrs_sheet <- "MYE4"
+
+# Name of the sheet with LA median ages from 2001 to 2020
+pop_median_age_sheet <- "MYE 6"
+
+# Name of the sheet with latest LA population by year of age
+pop_age_sheet <- "MYE2 - Persons"
+
+# Name of the sheet with latest LA male population by year of age
+pop_age_male_sheet <- "MYE2 - Males"
+
+# Name of the sheet with latest LA female population by year of age
+pop_age_female_sheet <- "MYE2 - Females"
+
+# # Name of the sheet with latest LA population by 5 year age groups
+# pop_age_band_sheet <- "MYE1"
+
+# READ -------------------------
+# Note: Manually downloaded from ONS for now
+
+pop_la_yrs <- read_xls(
+  path = str_c(data_in_folder, "/", pop_la_file),
+  sheet = pop_yrs_sheet,
+  skip = 7)
+
+pop_la_median_age <- read_xls(
+  path = str_c(data_in_folder, "/", pop_la_file),
+  sheet = pop_median_age_sheet,
+  skip = 7)
+
+pop_la_age <- read_xls(
+  path = str_c(data_in_folder, "/", pop_la_file),
+  sheet = pop_age_sheet,
+  skip = 7)
+
+pop_la_age_male <- read_xls(
+  path = str_c(data_in_folder, "/", pop_la_file),
+  sheet = pop_age_male_sheet,
+  skip = 7)
+
+pop_la_age_female <- read_xls(
+  path = str_c(data_in_folder, "/", pop_la_file),
+  sheet = pop_age_female_sheet,
+  skip = 7)
+
+# TRANSFORM ---------------------
+
+pop_la_yrs <-  pop_la_yrs %>%
+  filter(Name == "Sheffield") %>%
+  pivot_longer(cols = starts_with("Mid-"),
+               names_to = "Year",
+               values_to = "Population") %>%
+  select(Year, Population) %>%
+  mutate(Year = str_replace(Year, "Mid-", "")) %>%
+  mutate(Year = as.numeric(Year)) # or type year?
+
+pop_la_median_age <- pop_la_median_age %>%
+  filter(Name == "Sheffield") %>%
+  pivot_longer(cols = starts_with("Mid-"),
+               names_to = "Year",
+               values_to = "Median age") %>%
+  select(Year, `Median age`) %>%
+  mutate(Year = str_replace(Year, "Mid-", "")) %>%
+  mutate(Year = as.numeric(Year)) # or type year?
+
+pop_la_age <- pop_la_age %>%
+  filter(Name == "Sheffield") %>%
+  select(-Code, -Name, -Geography) %>%
+  pivot_longer(cols = everything(),
+               names_to = "Age",
+               values_to = "Persons")
+
+pop_la_age_male <- pop_la_age_male %>%
+  filter(Name == "Sheffield") %>%
+  select(-Code, -Name, -Geography) %>%
+  pivot_longer(cols = everything(),
+               names_to = "Age",
+               values_to = "Males")
+
+pop_la_age_female <- pop_la_age_female %>%
+  filter(Name == "Sheffield") %>%
+  select(-Code, -Name, -Geography) %>%
+  pivot_longer(cols = everything(),
+               names_to = "Age",
+               values_to = "Females")
+
+# WRITE --------------------------
+
+usethis::use_data(pop_la_yrs, compress = "xz", overwrite = T)
+
+usethis::use_data(pop_la_median_age, compress = "xz", overwrite = T)
+
+usethis::use_data(pop_la_age, compress = "xz", overwrite = T)
+
+usethis::use_data(pop_la_age_male, compress = "xz", overwrite = T)
+
+usethis::use_data(pop_la_age_female, compress = "xz", overwrite = T)
