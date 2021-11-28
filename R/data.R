68c9075f977b3f25dc478aab683c1ba0ecae23ae
laurie-platt <laurieplatt@hotmail.com>
2021-11-28 14:10:14 +0000

    start data documentation

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Population of Sheffield LSOAs by fifteen year age bands.
+#'
+#' Sheffield population provided by the ONS. 2020 mid-year estimates.
+#'
+#' @format A data frame with four variables: \code{LSOA Code}, \code{LSOA Name},
+#'   \code{Age band} and \code{Population}.
+"pop_lsoa_age_band"
